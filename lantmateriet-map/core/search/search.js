31453b7cabe5eef680ff1565a134bb126527adc4
hamedasemi <h-@live.se>
2018-11-20 13:22:47 +0100

    Add search component

---
+++ lantmateriet-map/core/search/search.js
@@ -0,0 +1,30 @@
+import { html, PolymerElement } from '../../node_modules/@polymer/polymer/polymer-element.js';
+
+export default class AppSearch extends PolymerElement {
+
+    static get template() {
+        return html`
+            <style include="shared-style">
+
+                :host {
+                    display: block;
+                    grid-area: search;
+                }
+
+            </style>
+                search
+            <slot></slot>
+        `
+    }
+
+    ready() {
+        super.ready()
+
+    }
+
+    static get properties() {
+        return {}
+    }
+}
+
+window.customElements.define('app-search', AppSearch)
