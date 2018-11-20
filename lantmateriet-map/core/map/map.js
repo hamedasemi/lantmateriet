175c51382de92a6bc34df9675be621d067eb6fb5
hamedasemi <h-@live.se>
2018-11-20 14:27:15 +0100

    CLean code

--- lantmateriet-map/core/map/map.js
+++ lantmateriet-map/core/map/map.js
@@ -22,7 +22,7 @@ export default class AppMap extends PolymerElement {
     ready() {
         super.ready()
 
-        var map = L.map(this).setView([51.505, -0.09], 13);
+        var map = L.map(this).setView([51.505, -0.09], 13)
 
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' }).addTo(map)
 
