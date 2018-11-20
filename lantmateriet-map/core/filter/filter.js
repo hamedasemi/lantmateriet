0e693be020f14bdd25d1c32f9b1902eb9fd91998
hamedasemi <h-@live.se>
2018-11-20 13:25:26 +0100

    Add filter component

---
+++ lantmateriet-map/core/filter/filter.js
@@ -0,0 +1,30 @@
+import { html, PolymerElement } from '../../node_modules/@polymer/polymer/polymer-element.js';
+
+export default class AppFilter extends PolymerElement {
+
+    static get template() {
+        return html`
+            <style include="shared-style">
+
+                :host {
+                    display: block;
+                    grid-area: filter;
+                }
+
+            </style>
+                filter
+            <slot></slot>
+        `
+    }
+
+    ready() {
+        super.ready()
+
+    }
+
+    static get properties() {
+        return {}
+    }
+}
+
+window.customElements.define('app-filter', AppFilter)
