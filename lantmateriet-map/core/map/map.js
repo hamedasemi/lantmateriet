f7a59127870567b21c8a08ff35d99a3d6c7558dc
hamedasemi <h-@live.se>
2018-11-27 11:45:23 +0100

    Add basic geo json

--- lantmateriet-map/core/map/map.js
+++ lantmateriet-map/core/map/map.js
@@ -22,15 +22,31 @@ export default class AppMap extends PolymerElement {
     ready() {
         super.ready()
 
-        var map = L.map(this).setView([51.505, -0.09], 13)
+        var map = L.map(this).setView([61.6794500443896, 16.375], 5)
+
+        map.on('zoom', (data) => {
+            console.log('zoom', data.target._zoom)
+        })
 
         L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>' }).addTo(map)
 
-        L.marker([51.505, -0.09]).addTo(map)
-            .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        fetch(new Request('lantmateriet-map/core/map/data/municipalities.json'))
+            .then(response => {
+                if (response.status === 200) {
+                    return response.json();
+                } else {
+                    throw new Error('Something went wrong on api server!');
+                }
+            })
+            .then(response => {
+                L.geoJSON(response, {
+                }).addTo(map);
 
-        window.dispatchEvent(new Event('resize'))
+                window.dispatchEvent(new Event('resize'))
 
+            }).catch(error => {
+                console.error(error);
+            });
     }
 
     static get properties() {
