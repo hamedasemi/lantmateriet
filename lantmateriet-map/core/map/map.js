5ffb5f8f2146add79822ee11767884e834cfc1c8
hamedasemi <h-@live.se>
2018-11-27 12:49:02 +0100

    Add zoom level limit

--- lantmateriet-map/core/map/map.js
+++ lantmateriet-map/core/map/map.js
@@ -24,29 +24,40 @@ export default class AppMap extends PolymerElement {
 
         var map = L.map(this).setView([61.6794500443896, 16.375], 5)
 
-        map.on('zoom', (data) => {
-            console.log('zoom', data.target._zoom)
-        })
-
         L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>' }).addTo(map)
 
-        fetch(new Request('lantmateriet-map/core/map/data/municipalities.json'))
-            .then(response => {
-                if (response.status === 200) {
-                    return response.json()
+        let yourUserDefinedLayerNameGoesHere;
+
+        map.on('zoomend', (data) => {
+            console.log('zoom', data.target._zoom)
+
+            if (7 <= data.target._zoom && data.target._zoom <= 14) {
+                if (!map.hasLayer(yourUserDefinedLayerNameGoesHere)) {
+                    fetch(new Request('lantmateriet-map/core/map/data/municipalities.json'))
+                        .then(response => {
+                            if (response.status === 200) {
+                                return response.json()
+                            } else {
+                                throw new Error('Something went wrong on api server!')
+                            }
+                        })
+                        .then(response => {
+                            yourUserDefinedLayerNameGoesHere = L.geoJSON(response, {
+                            }).addTo(map)
+
+
+
+                        }).catch(error => {
+                            console.error(error)
+                        })
                 } else {
-                    throw new Error('Something went wrong on api server!')
+
                 }
-            })
-            .then(response => {
-                L.geoJSON(response, {
-                }).addTo(map)
-
-                window.dispatchEvent(new Event('resize'))
-
-            }).catch(error => {
-                console.error(error)
-            })
+            } else {
+                map.removeLayer(yourUserDefinedLayerNameGoesHere)
+            }
+        })
+        window.dispatchEvent(new Event('resize'))
     }
 
     static get properties() {
