7d4735ab106e5a1b5e00d59ec528d952f1ee8bf3
hamedasemi <h-@live.se>
2018-11-28 13:33:08 +0100

    Add border radius

--- lantmateriet-map/core/menu/menu.js
+++ lantmateriet-map/core/menu/menu.js
@@ -27,6 +27,8 @@ export default class AppMenu extends PolymerElement {
                     width: 60px;
                     height: 60px;
                     background: orange;
+                    border-top-right-radius: 4px;
+                    border-bottom-right-radius: 4px;
                 }
                 svg {
                     height: 50%;
