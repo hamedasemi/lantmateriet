502bf26e942082f47703a0ff5a2881f8f44b274c
hamedasemi <h-@live.se>
2018-11-29 21:15:22 +0100

    More style implemented

--- lantmateriet-map/shared/drop-down/drop-down.js
+++ lantmateriet-map/shared/drop-down/drop-down.js
@@ -10,22 +10,30 @@ export default class AppDropDown extends PolymerElement {
 
                 :host {
                     display: block;
+                    position: relative;
+                    
                 }
-
+                
                 :host > [selected] {
-                    border: 1px solid black;
-                    border-radius: 4px;
                     background-color: #f8f8f8;
                     padding: 11px;
                     display: block;
                     white-space: nowrap;
                     overflow: hidden;
                     text-overflow: ellipsis;
+                    border: 1px solid black;
+                    border-top-right-radius: 4px;
+                    border-top-left-radius: 4px;
                 }
                 [options] {
+                    z-index: 1;
                     display: none;
                     position: absolute;
                     background-color: #f8f8f8;
+                    padding: 11px;
+                    width: 100%;
+                    box-shadow: 0px 5px 17px -5px;
+                    border: 1px solid black;
                 }
                 [active][options] {
                     display: flex;
@@ -42,7 +50,11 @@ export default class AppDropDown extends PolymerElement {
             <div selected on-click="selectedClick">
                 <dom-if if="[[!getSelectedStats(options)]]">
                     <template>
-                        [[placeholder]]
+                        [[placeholder]] 
+                        <svg xmlns="http://www.w3.org/2000/svg" width="60" height="35" viewBox="0 0 60 35">
+                            <path fill="#333" fill-rule="evenodd" d="M57.236 6.4a3.236 3.236 0 0 0 0-4.582 3.2 3.2 0 0 0-4.509 0L29.091 25.455 5.455 1.818a3.2 3.2 0 0 0-4.564 0A3.164 3.164 0 0 0 0 4.11 3.2 3.2 0 0 0 .945 6.4l28.146 28.145L57.236 6.4z"/>
+                        </svg>
+
                     </template>
                 </dom-if>
                 <dom-repeat items="[[options]]" as="option">
