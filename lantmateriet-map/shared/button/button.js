e0e46a76d23c54ea3a6152f827bcac9d50dddabb
hamedasemi <h-@live.se>
2018-12-17 11:00:11 +0100

    Add app button

---
+++ lantmateriet-map/shared/button/button.js
@@ -0,0 +1,31 @@
+import { html, PolymerElement } from '../../../node_modules/@polymer/polymer/polymer-element.js'
+
+export default class AppButton extends PolymerElement {
+
+    static get is () { return 'app-button'}
+
+    static get template() {
+        return html`
+            <style include="shared-style">
+                :host {
+                    display: block;
+                    border-radius: 4px;
+                    box-shadow: 0 1px 4px 1px rgba(0, 0, 0, 0.5);
+                    background-color: #ffffff;
+                    padding: 2rem;
+                    text-align: center;
+                    cursor: pointer;
+                }
+
+            </style>
+            <slot></slot>
+        `
+    }
+
+    static get properties() {
+        return {}
+    }
+
+}
+
+window.customElements.define(AppButton.is, AppButton)
